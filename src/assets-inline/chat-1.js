f9acedc08124086c359ed3820233e6c52e5de833
Shunikai972 <noammsakni@gmail.com>
2026-08-19 16:43:24 +0200

    style: load glass redesign overrides

--- src/assets-inline/chat-1.js
+++ src/assets-inline/chat-1.js
@@ -1,1 +1,9 @@
+const id = 'noam-glass-redesign'
+if (!document.getElementById(id)) {
+  const link = document.createElement('link')
+  link.id = id
+  link.rel = 'stylesheet'
+  link.href = './src/glass-overrides.css?v=glass-3d-v2'
+  document.head.appendChild(link)
+}
 export { default } from './chat-2.js'
